a5f63e745b9f3591bfa7a9c77264bc7b3725323b
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-26 19:35:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = []
+  languages.each do |item|
+    if item[:style] == [:oo]
+      new_hash[:oo] == item
+    end
+  end
+  return new_hash
 end
